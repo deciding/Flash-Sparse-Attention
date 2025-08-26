000d253d5db343be1534191f16cadf243dd50fd1
Ran-Yan-HK <ranyanpku@gmail.com>
2025-08-26 12:21:11 +0000

    fix

--- scripts/run_unit_test_sel_attn.sh
+++ scripts/run_unit_test_sel_attn.sh
@@ -7,8 +7,9 @@ for seqlen in 8192 16384 32768 65536; do
     CUDA_VISIBLE_DEVICES=${device} python3 'test/test_FSA_optimized_sel_attn.py' \
         --seqlen ${seqlen} \
         --num-q-heads 32 \
-        --num-k-heads 4 \
+        --num-k-heads 32 \
         --head-dim 128 \
         --block-size 64 \
-        --topk 16
+        --topk 16 \
+        --benchmark-iters 20
 done
